abcbda36cdd102ccfcbe18e92d8b9e16dda9eef3
Amulya <amulyakammampati@gmail.com>
2025-03-16 00:19:35 +0530

    index

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import App16 from './App16';
-import App17 from './App17';
+import App18 from './App18';
 
-const sqrfunction= (n) =>{
-    return n*n;
-}
+// const sqrfunction= (n) =>{
+//     return n*n;
+// }
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(<App flag = {false} />);
 // root.render(<Main1 flag={true} />);
 // root.render(<App14 marks={[34,21,56]}/>)
 // root.render(<App15 result = {{"john":40,"cathy":50}} />);
-root.render(
-  <>
-     <App16 val={5} fn={sqrfunction}/>
-     <App17 val={7} fn={sqrfunction}/>
-</>
-);
+// root.render(
+//   <>
+//      <App16 val={5} fn={sqrfunction}/>
+//      <App17 val={7} fn={sqrfunction}/>
+// </>
+// );
+root.render(<App18/>)
